69dbb4ce410b6d1c003e0432f5453ae4af37ac7c
Jonas Bergqvist <jonas.bergqvist@episerver.com>
2024-03-06 17:07:25 +0100

    cool stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useTotalItemsQuery } from "./../generated"
+import { Ranking, useArtistSearchQuery } from "./../generated"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
-  const totalItemsResult = useTotalItemsQuery();
+  const result = useArtistSearchQuery({ variables: { searchPhrase: "Metallica", relevance: Ranking.Semantic } });
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      Total { totalItemsResult.data?.Data?.total }
+      <ul>
+      { result.data?.Artist?.facets?.Genre?.map(x => {
+        return (
+            <li>{ x?.name } ({x?.count})</li>
+        )
+      })}
+      </ul>
+      <div>
+      { result.data?.Artist?.items?.map(x => {
+        return (
+          <div>
+            <div>{ x?.Artist_Name }</div>
+            <div>{ x?.Genre }</div>
+            <div>{ x?.Biggest_Hits }</div>
+            <div>{ x?.Teaser_Text }</div>
+          </div>
+        )
+      })}
+      </div>
     </main>
   );
 }
